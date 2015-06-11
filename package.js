780aaeec652a4ec11ecac588ef320d83c2c7ce6e
James Gillmore <james@faceyspacey.com>
2015-06-11 05:00:22 -0700

    fix addedHooks

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Ultimate MVC - The Ultimate Toolset of Uniform Classes for Meteor",
-  version: "0.0.11",
+  version: "0.0.12",
   git: "https://github.com/ultimate-ide/meteor-ultimate-models.git",
   name: "ultimateide:ultimate-mvc"
 });
