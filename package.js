5fd2709b3712c960c7cfbe4c11ad128f1c07e7f1
James Gillmore <james@faceyspacey.com>
2015-06-11 13:24:18 -0700

    made relations/subs/aggs inherit properly

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Ultimate MVC - The Ultimate Toolset of Uniform Classes for Meteor",
-  version: "0.0.12",
+  version: "0.0.13",
   git: "https://github.com/ultimate-ide/meteor-ultimate-models.git",
   name: "ultimateide:ultimate-mvc"
 });
