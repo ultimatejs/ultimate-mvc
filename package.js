e1d9d5f496f2e62c416825efdc125da5db4660a2
James Gillmore <james@faceyspacey.com>
2015-06-19 18:35:29 -0700

    ultimateconfig minor changes

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Ultimate MVC - The Ultimate Toolset of Uniform Classes for Meteor",
-  version: "0.0.16",
+  version: "0.0.17",
   git: "https://github.com/ultimate-ide/meteor-ultimate-models.git",
   name: "ultimateide:ultimate-mvc"
 });
